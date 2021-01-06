b0197f7d4036f417ed4c731136737a45a2694e63
Cristian Prieto <me@cprieto.com>
2021-01-06 10:47:41 +0100

    Add lesson 24, write back to socket

---
+++ lesson34.asm
@@ -0,0 +1,71 @@
+%include 'functions.asm'
+%include 'syscalls.asm'
+
+section .bss
+  handler resd  1
+  buffer  resb  255
+
+section .text
+  global _start
+
+_start:
+  push byte IPPROTO_TCP
+  push byte SOCK_STREAM
+  push byte AF_INET
+  mov eax, SYS_SOCKETCALL
+  mov ebx, SOCKET_OPEN
+  mov ecx, esp
+  int 80h
+
+  mov [handler], eax
+
+_bind:
+  push dword 0
+  push word 0x2923
+  push word AF_INET
+  mov ecx, esp
+  push byte 16
+  push ecx
+  push dword [handler]
+  mov eax, SYS_SOCKETCALL
+  mov ebx, SOCKET_BIND
+  mov ecx, esp
+  int 80h
+
+_listen:
+  push byte 1
+  push dword [handler]
+  mov eax, SYS_SOCKETCALL
+  mov ebx, SOCKET_LISTEN
+  mov ecx, esp
+  int 80h
+
+_accept:
+  push byte 0
+  push byte 0
+  push dword [handler]
+  mov eax, SYS_SOCKETCALL
+  mov ebx, SOCKET_ACCEPT
+  mov ecx, esp
+  int 80h
+
+_fork: ; Why do we have to fork?
+  mov edi, eax
+  mov eax, SYS_FORK
+  int 80h
+
+  cmp eax, 0
+  jz _read
+  jmp _accept
+
+_read:
+  mov eax, SYS_READ
+  mov ebx, edi
+  mov ecx, buffer
+  mov edx, 255
+  int 80h
+
+  mov eax, buffer
+  call sprintn
+
+  call quit
---
+++ syscalls.asm
@@ -0,0 +1,13 @@
+%define SYS_FORK 2
+%define SYS_READ 3
+%define SYS_SOCKETCALL 102
+
+; socket operations
+%define SOCKET_OPEN 1
+%define SOCKET_BIND 2
+%define SOCKET_LISTEN 4
+%define SOCKET_ACCEPT 5
+
+%define IPPROTO_TCP 6
+%define SOCK_STREAM 1
+%define AF_INET 2
